b0edf48ba5830143e6c01e35d68ee18bfc8f7e11
RoseECooper <rose.cooper@stfc.ac.uk>
2021-05-27 16:20:29 +0100

    Changed comments and print statement

--- basics/Hello world.py
+++ basics/Hello world.py
@@ -1,10 +1,6 @@
 #Classic hello world program
 print("Hello world ")
-print("Test") 
+print("Test")
 
-#make a random change do test how git works
-#not actually adding anything of value tot he code though!
-
-print("okuurrrrr")
-
-#lol
+#even more random changes!!
+print("yaas queen")
