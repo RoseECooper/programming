4b2e2539b61314c8985440a6a51613a1904c1afb
RoseECooper <rose.cooper@stfc.ac.uk>
2021-06-16 11:27:42 +0100

    branch testing

--- basics/Hello world.py
+++ basics/Hello world.py
@@ -7,3 +7,9 @@ print("Test")
 
 print("I love drag race")
 #I'm running out of random generic print statements, can you tell! 
+
+#I managed to do what I wanted by accident, and I don't really know how or what I did to get there
+#So now I need to try and work out what I did!!
+#Just more random print statements lol 
+
+print("I also love D&D")
