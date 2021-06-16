011657f98ba6dddab15e5eb361caad96022268f4
RoseECooper <rose.cooper@stfc.ac.uk>
2021-06-16 11:07:03 +0100

    test

--- basics/Hello world.py
+++ basics/Hello world.py
@@ -1,3 +1,9 @@
 #Classic hello world program
 print("Hello world ")
 print("Test")
+
+#still trying to work out what Im doing with branches!! 
+#hoping these changes stay on test!
+
+print("I love drag race")
+#I'm running out of random generic print statements, can you tell! 
---
+++ basics/qwik_maffs.py
@@ -0,0 +1,9 @@
+#Just some basic maths bits.
+#Trying to work out how to interact with variables from a python file in the terminal after the program has been run.
+#Basically just giving myself some variables and values to play with!
+
+a=4+2
+b=3*5
+c=10-2
+
+print(a, b, c)
